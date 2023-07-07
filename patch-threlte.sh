26ac606de13948002ae2c83fe735ba8b7f0f0665
Jonathan Lo <jonathanlo411@gmail.com>
2023-07-07 13:20:11 -0700

    add patch script

---
+++ patch-threlte.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+filepath="node_modules/three/examples/jsm/objects"
+target="GroundProjectedSkybox"
+replace="GroundProjectedEnv"
+
+sed "s/$target/$replace/" "$filepath/$target.js" > "$filepath/$replace.js"
